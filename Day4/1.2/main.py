757da25484d760e0e3aff4c8ec8d37242a01d225
TELangelaar <t.e.langelaar@student.rug.nl>
2021-12-26 13:11:43 +0100

    Day4 - 1.2 added

--- Day4/1.2/main.py
+++ Day4/1.2/main.py
@@ -1,6 +1,7 @@
 from typing import List
 import copy
 
+
 def parse_input(file_name: str) -> (List[int], dict):
     with open(file_name, 'r') as file:
         drawn_numbers = file.readline()
@@ -23,30 +24,47 @@ def parse_input(file_name: str) -> (List[int], dict):
 
 
 def find_final_score(drawn_numbers: List[int], boards: dict) -> int:
-    has_won_list = []
+    bingo = []
 
     copy_boards = copy.deepcopy(boards)
     lastdrawn = -1
     number_of_boards = len(boards)
     for drawn in drawn_numbers:
         for board in boards.keys():
+            # transponeren van matrix
+            vert_bingo = []
+            for ii in range(0, len(boards[board][0])):
+                vert_bingo.append([])
+                for jj in range(0, len(boards[board])):
+                    vert_bingo[ii].append(copy_boards[board][jj][ii])
+
             for idx, row in zip(range(0, len(boards[board])), boards[board]):
-                if len(row) == 0:
-                    has_won_list.append(board)
-                if len(set(has_won_list)) == number_of_boards:
-                    return sum_unmarked_elements(boards[board]) * lastdrawn
-                for element in row:
+                # horizontal bingo
+                if len(remove_none(copy_boards[board][idx])) == 0:
+                    bingo.append(board)
+                # vertical bingo
+                if len(remove_none(vert_bingo[idx])) == 0:
+                    bingo.append(board)
+
+                if len(set(bingo)) == number_of_boards:
+                    return sum_unmarked_elements(copy_boards[board]) * lastdrawn
+
+                for idx3, element in zip(range(0, len(row)), row):
                     if drawn == element:
-                        copy_boards[board][idx].remove(element)
+                        copy_boards[board][idx][idx3] = None
         lastdrawn = drawn
     return -1
 
 
-def sum_unmarked_elements(board: List[int]) -> int:
+def sum_unmarked_elements(board: List[List[int]]) -> int:
     complete_sum = 0
     for row in board:
-        complete_sum += sum(row)
+        complete_sum += sum(remove_none(row))
     return complete_sum
+
+
+def remove_none(lst):
+    return [x for x in lst if x is not None]
 
 
 if __name__ == '__main__':
