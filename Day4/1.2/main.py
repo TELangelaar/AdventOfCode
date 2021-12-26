62588243de6ad14f2ea04cf3ddb5e05ca8b36c72
TELangelaar <t.e.langelaar@student.rug.nl>
2021-12-26 11:03:07 +0100

    Day 1 - Day 4

---
+++ Day4/1.2/main.py
@@ -0,0 +1,55 @@
+from typing import List
+import copy
+
+def parse_input(file_name: str) -> (List[int], dict):
+    with open(file_name, 'r') as file:
+        drawn_numbers = file.readline()
+        drawn_numbers_list = drawn_numbers.split(',')
+        drawn_numbers_list = [int(x) for x in drawn_numbers_list]
+
+        boards_raw = file.read().splitlines()
+        boards = {}
+        board_number = 0
+        for string in boards_raw:
+            if string == '':
+                board_number += 1
+            else:
+                row = string.split(' ')
+                row = [int(x) for x in row if x != '']
+                if board_number not in boards:
+                    boards[board_number] = []
+                boards[board_number].append(row)
+        return drawn_numbers_list, boards
+
+
+def find_final_score(drawn_numbers: List[int], boards: dict) -> int:
+    has_won_list = []
+
+    copy_boards = copy.deepcopy(boards)
+    lastdrawn = -1
+    number_of_boards = len(boards)
+    for drawn in drawn_numbers:
+        for board in boards.keys():
+            for idx, row in zip(range(0, len(boards[board])), boards[board]):
+                if len(row) == 0:
+                    has_won_list.append(board)
+                if len(set(has_won_list)) == number_of_boards:
+                    return sum_unmarked_elements(boards[board]) * lastdrawn
+                for element in row:
+                    if drawn == element:
+                        copy_boards[board][idx].remove(element)
+        lastdrawn = drawn
+    return -1
+
+
+def sum_unmarked_elements(board: List[int]) -> int:
+    complete_sum = 0
+    for row in board:
+        complete_sum += sum(row)
+    return complete_sum
+
+
+if __name__ == '__main__':
+    drawn_numbers, boards = parse_input('Day4/1.2/input.txt')
+    score = find_final_score(drawn_numbers, boards)
+    print(score)
