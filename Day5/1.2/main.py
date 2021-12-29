7fdd11cbfd4f2a119ec6dd8f4bf2c880a31ead09
TELangelaar <t.e.langelaar@student.rug.nl>
2021-12-29 10:21:06 +0100

    Day 6 until Day 9

--- Day5/1.2/main.py
+++ Day5/1.2/main.py
@@ -1,4 +1,5 @@
 from typing import List
+import pprint
 
 
 def parse_input(file_name: str) -> List[List[int]]:
@@ -18,27 +19,39 @@ def fill_diagram(co, dia=None):
         x1, y1 = co[idx_pair][0], co[idx_pair][1]
         x2, y2 = co[idx_pair + 1][0], co[idx_pair + 1][1]
 
-        if x1 == x2:
+        diff_x, diff_y = abs(x1-x2), abs(y1-y2)
+        if diff_x == diff_y:
+            x_smallest = min((x1, y1), (x2, y2), key=lambda x: x[0])
+            x_biggest = max((x1, y1), (x2, y2), key=lambda x: x[0])
+            if x_smallest[1] < x_biggest[1]:
+                for i, j in zip(range(x_smallest[0], x_biggest[0] + 1), range(x_smallest[1], x_biggest[1] + 1)):
+                    place_dia_value(dia, i, j)
+            else:
+                for i, j in zip(range(x_smallest[0], x_biggest[0] + 1), range(x_smallest[1], x_biggest[1] - 1, -1)):
+                    place_dia_value(dia, i, j)
+
+        elif x1 == x2:
             y_smallest = min(y1, y2)
             y_biggest = max(y1, y2)
             ys = [x for x in range(y_smallest, y_biggest + 1)]
             for y in ys:
-                if dia[y][x1] == '.':
-                    dia[y][x1] = '1'
-                else:
-                    current = int(dia[y][x1])
-                    dia[y][x1] = str(current + 1)
+                place_dia_value(dia, x1, y)
         elif y1 == y2:
             x_smallest = min(x1, x2)
             x_biggest = max(x1, x2)
             xs = [x for x in range(x_smallest, x_biggest + 1)]
             for x in xs:
-                if dia[y1][x] == '.':
-                    dia[y1][x] = '1'
-                else:
-                    current = int(dia[y1][x])
-                    dia[y1][x] = str(current + 1)
+                place_dia_value(dia, x, y1)
+
     return dia
+
+
+def place_dia_value(dia, x, y):
+    if dia[y][x] == '.':
+        dia[y][x] = '1'
+    else:
+        current = int(dia[y][x])
+        dia[y][x] = str(current + 1)
 
 
 def get_empty_diagram(size=9) -> [List[List[str]]]:
@@ -46,7 +59,7 @@ def get_empty_diagram(size=9) -> [List[List[str]]]:
 
 
 if __name__ == "__main__":
-    coordinates = parse_input('Day5/1.1/input.txt')
+    coordinates = parse_input('Day5/1.2/input.txt')
     diagram = get_empty_diagram(max(list(map(max, coordinates))))
     fill_diagram(coordinates, diagram)
     n_overlapping_coordinates = [1 for y in diagram for x in y if x != '.' and int(x) > 1]
--- Day6/1.1/main.py
+++ Day6/1.1/main.py
@@ -1,53 +1,48 @@
 from typing import List
 
 
-def parse_input(file_name: str) -> List[List[int]]:
+def parse_input(file_name: str) -> List[int]:
     with open(file_name, 'r') as file:
-        co = file.read().splitlines()
-        co = [x.split(' -> ') for x in co]
-        co = [x.split(',') for pair in co for x in pair]
-        co = [list(map(int, x)) for x in co]
-        return co
+        start_states = file.read().split(',')
+        start_states = [int(x) for x in start_states]
+        return start_states
 
 
-def fill_diagram(co, dia=None):
-    if dia is None:
-        dia = get_empty_diagram()
-
-    for idx_pair in range(0, len(co)-1, 2):
-        x1, y1 = co[idx_pair][0], co[idx_pair][1]
-        x2, y2 = co[idx_pair + 1][0], co[idx_pair + 1][1]
-
-        if x1 == x2:
-            y_smallest = min(y1, y2)
-            y_biggest = max(y1, y2)
-            ys = [x for x in range(y_smallest, y_biggest + 1)]
-            for y in ys:
-                if dia[y][x1] == '.':
-                    dia[y][x1] = '1'
-                else:
-                    current = int(dia[y][x1])
-                    dia[y][x1] = str(current + 1)
-        elif y1 == y2:
-            x_smallest = min(x1, x2)
-            x_biggest = max(x1, x2)
-            xs = [x for x in range(x_smallest, x_biggest + 1)]
-            for x in xs:
-                if dia[y1][x] == '.':
-                    dia[y1][x] = '1'
-                else:
-                    current = int(dia[y1][x])
-                    dia[y1][x] = str(current + 1)
-    return dia
+def get_number_of_fish(st, number_of_days):
+    for day in range(0, number_of_days):
+        for i in range(0, len(st)):
+            if st[i] == 0:
+                st[i] = 6
+                st.append(8)
+            else:
+                st[i] -= 1
+    return len(st)
 
 
-def get_empty_diagram(size=9) -> [List[List[str]]]:
-    return [['.' for _ in range(0, size + 1)] for _ in range(0, size + 1)]
+def get_number_of_fish_optimized(st, number_of_days):
+    result = dict([(k, 0) for k in range(0, 9)])
+    for i in range(0, len(st)):
+        result[st[i]] += 1
+
+    for day in range(0, number_of_days):
+        for j in range(len(result) - 1, -1, -1):
+            if j == 8:
+                previous = result[j]
+                result[j] = result[0]
+            elif j == 0:
+                result[8] = result[j]
+                result[6] += result[j]
+                result[j] = previous
+            else:
+                current = result[j]
+                result[j] = previous
+                previous = current
+
+    return result
 
 
 if __name__ == "__main__":
-    coordinates = parse_input('Day5/1.1/input.txt')
-    diagram = get_empty_diagram(max(list(map(max, coordinates))))
-    fill_diagram(coordinates, diagram)
-    n_overlapping_coordinates = [1 for y in diagram for x in y if x != '.' and int(x) > 1]
-    print(f"number of overlapping points: {sum(n_overlapping_coordinates)}")
+    start_timers = parse_input('Day6/1.1/input.txt')
+    number_of_fish = get_number_of_fish_optimized(start_timers, 256)
+    print(number_of_fish)
+    print(sum(number_of_fish.values()))
