b99792cf9fced323d4784bb838d26476be0da32c
Thijme Langelaar <t.langelaar@brink.nl>
2023-12-16 21:29:44 +0100

    Day5 part2 wip

--- 2023/rust/Day5/fertilizer/src/main.rs
+++ 2023/rust/Day5/fertilizer/src/main.rs
@@ -1,11 +1,11 @@
-use std::{collections::HashMap, time::Instant, ops::Range};
+use std::{collections::HashMap, iter::zip, ops::Range, time::Instant};
 
 use nom::{
     bytes::complete::{tag, take},
     character::complete::{digit1, line_ending},
     combinator::{map, map_res},
-    multi::{many1, separated_list1, many_till},
-    sequence::{preceded, tuple},
+    multi::{many1, many_till, separated_list1},
+    sequence::{preceded, separated_pair, tuple},
     IResult,
 };
 
@@ -55,24 +55,28 @@ humidity-to-location map:
 
     let almanac = Almanac::parse(&result).unwrap().1;
     let locations = almanac.to_locations();
-    println!("Lowest location number: {:#?}", locations.iter().min().unwrap());
+    println!(
+        "Lowest location number: {:#?}",
+        locations.iter().min().unwrap()
+    );
 }
 
 #[derive(Debug)]
 struct Seeds {
-    values: Vec<usize>,
+    values: Vec<Range<usize>>,
 }
 
 #[derive(Debug)]
 struct Mapping {
     differences: Vec<isize>,
-    source_ranges: Vec<Range<usize>>
+    source_ranges: Vec<Range<usize>>,
+    dest_ranges: Vec<Range<usize>>
 }
 
 #[derive(Debug)]
 struct Almanac {
     seeds: Seeds,
-    mappings: Vec<Mapping>
+    mappings: Vec<Mapping>,
 }
 
 impl Mapping {
@@ -80,63 +84,142 @@ impl Mapping {
         let (input, _) = many1(line_ending)(input)?;
         let (input, _) = many_till(take(1usize), line_ending)(input)?;
 
-        let map = tuple((map_res(digit1, |s: &str| s.parse::<usize>()), tag(" "), map_res(digit1, |s: &str| s.parse::<usize>()), tag(" "), map_res(digit1, |s: &str| s.parse::<usize>())));
+        let map = tuple((
+            map_res(digit1, |s: &str| s.parse::<usize>()),
+            tag(" "),
+            map_res(digit1, |s: &str| s.parse::<usize>()),
+            tag(" "),
+            map_res(digit1, |s: &str| s.parse::<usize>()),
+        ));
         let (input, mappings) = separated_list1(line_ending, map)(input)?;
 
-        Ok((input, mappings.into_iter().map(|(dest, _, source, _, range)| (dest, source, range)).collect()))
+        Ok((
+            input,
+            mappings
+                .into_iter()
+                .map(|(dest, _, source, _, range)| (dest, source, range))
+                .collect(),
+        ))
     }
 
     fn create_map(mappings: Vec<(usize, usize, usize)>) -> Mapping {
         let mut differences = vec![];
         let mut source_ranges = vec![];
+        let mut dest_ranges = vec![];
         for (dest, source, range) in mappings {
-            dbg!(source, dest);
             differences.push(source as isize - dest as isize);
-            source_ranges.push(source..(source+range));
+            source_ranges.push(source..(source + range));
+            dest_ranges.push(dest..(dest+range));
         }
 
-        Mapping { differences, source_ranges}
+        Mapping {
+            differences,
+            source_ranges,
+            dest_ranges
+        }
     }
 }
 
 impl Seeds {
     fn parse(input: &str) -> IResult<&str, Seeds> {
-        let (input, values) = preceded(tag("seeds:"), many1(preceded(tag(" "), map_res(digit1, |s: &str| s.parse::<usize>()))))(input)?;
+        let (input, pairs) = preceded(
+            tag("seeds:"),
+            many1(preceded(
+                tag(" "),
+                separated_pair(
+                    map_res(digit1, |s: &str| s.parse::<usize>()),
+                    tag(" "),
+                    map_res(digit1, |s: &str| s.parse::<usize>()),
+                ),
+            )),
+        )(input)?;
 
-        Ok((input, Seeds { values }))
+        let seeds = Seeds {
+            values: pairs
+                .into_iter()
+                .map(|(start, length)| start..(start + length))
+                .collect(),
+        };
+
+        Ok((input, seeds))
     }
 }
 
 impl Almanac {
     fn to_locations(&self) -> Vec<usize> {
-        let mut locations = vec![];
-        for seed in &self.seeds.values {
-            let mut location = *seed;
-            for mapping in &self.mappings {
-                if let Some((index, _)) = mapping.source_ranges.iter().enumerate().find(|(_, s)| s.contains(&location)) {
-                    let difference = *mapping.differences.get(index).unwrap();
-                    location = ((location as isize) - difference) as usize;
+        // let mut locations = vec![];
+        // for seed_range in &self.seeds.values {
+        //     for seed in seed_range.start..seed_range.end {
+        //         let mut location = seed;
+        //         for mapping in &self.mappings {
+        //             if let Some((index, _)) = mapping.source_ranges.iter().enumerate().find(|(_, s)| s.contains(&location)) {
+        //                 let difference = *mapping.differences.get(index).unwrap();
+        //                 location = ((location as isize) - difference) as usize;
+        //             }
+        //         }
+        //         locations.push(location);
+        //     }
+        // }
+        let mut sorted_locations = &self.mappings.last().unwrap().dest_ranges;
+        sorted_locations.sort_unstable_by(|first, second| {
+            first.start.partial_cmp(&second.start).unwrap()
+        });
+
+        let reversed_mappings = self.mappings.iter().rev().collect::<Vec<&Mapping>>();
+        for location_range in sorted_locations {
+            for humidity in
+                ((location_range.start as isize) - diff)..((location_range.end as isize) - diff)
+            {
+                let mut seed = 0usize;
+                for (idx, &mapping) in reversed_mappings.iter().enumerate() {
+                    if idx == 0 {
+                        continue;
+                    }
+                    // if let Some((index, _)) = mapping.source_ranges.iter().enumerate().find(|(_, s)| {((s.start as isize) - )}) {
+
+                    // }
                 }
             }
-            locations.push(location);
         }
 
-        locations
+        todo!()
     }
 
     fn parse(input: &str) -> IResult<&str, Almanac> {
         let (input, seeds) = Seeds::parse(input)?;
 
-        let (input, seed_to_soil) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
-        let (input, soil_to_fertilizer) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
-        let (input, fertilizer_to_water) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
-        let (input, water_to_light) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
-        let (input, light_to_temperature) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
-        let (input, temperature_to_humidity) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
-        let (input, humidity_to_location) = map(Mapping::parse_mapping, |mappings| Mapping::create_map(mappings))(input)?;
+        let (input, seed_to_soil) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
+        let (input, soil_to_fertilizer) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
+        let (input, fertilizer_to_water) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
+        let (input, water_to_light) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
+        let (input, light_to_temperature) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
+        let (input, temperature_to_humidity) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
+        let (input, humidity_to_location) = map(Mapping::parse_mapping, |mappings| {
+            Mapping::create_map(mappings)
+        })(input)?;
 
-        let mappings = vec![seed_to_soil, soil_to_fertilizer, fertilizer_to_water, water_to_light, light_to_temperature, temperature_to_humidity, humidity_to_location];
-        
+        let mappings = vec![
+            seed_to_soil,
+            soil_to_fertilizer,
+            fertilizer_to_water,
+            water_to_light,
+            light_to_temperature,
+            temperature_to_humidity,
+            humidity_to_location,
+        ];
+
         Ok((input, Almanac { seeds, mappings }))
     }
 }
